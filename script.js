ff719f32fe22fa56a9b13f98ff5d3f11b5d12e79
Atmaram Korgaonkar <120163458+viraj0510@users.noreply.github.com>
2025-03-31 20:48:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle mobile menu
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
